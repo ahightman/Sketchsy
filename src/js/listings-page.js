132e0ecb4dece914fb9a25fc6877efd9333d4b5a
Ashley Hightman <ahightman@gmail.com>
2015-06-02 23:01:22 -0400

    gh-pages

--- src/js/listings-page.js
+++ src/js/listings-page.js
@@ -4,9 +4,33 @@ app.router.add('', function () {
   app.etsy.listings()
     .done(function (data) {
       $('.cards').html(listTemplate({ items: data.results }));
+      $('.artist-info').click(function(){
+        var card = $(this).closest('.card');
+
+        getProfile(card);
+      })
     })
     .fail(function (req, status, err) {
       console.log(err);
       $('.cards').text(err.error);
     });
+
+  function getProfile(card) {
+    // var backofcard = $('.backofcard', card);
+    if (!card.hasClass('downloading')) {
+      card.addClass('downloading');
+      var profileTemplate = _.template($('#etsyProfile').html(), { variable: 'm' });
+
+      app.etsy.userDetail(card.attr('id'))
+        .done(function (data) {
+          card.append(profileTemplate(data.results[0]));
+        })
+        .fail(function (req, status, err) {
+          console.log('Failed');
+          console.log(err);
+          $('.cards').html(err.error);
+        });
+    }
+  }
+
 });
--- src/js/profile-page.js
+++ src/js/profile-page.js
@@ -1,16 +1,16 @@
 function test () {
-app.router.add('profiles/:userId', function (url) {
-  var profileTemplate = _.template($('#etsyProfile').html(), { variable: 'm' });
+  app.router.add('profiles/:userId', function (url) {
+    var profileTemplate = _.template($('#etsyProfile').html(), { variable: 'm' });
 
-  app.etsy.userDetail(url.params.userId)
-    .done(function (data) {
-      $('.cards').html(profileTemplate(data.results[0]));
-    })
-    .fail(function (req, status, err) {
-      console.log('Failed');
-      console.log(err);
-      $('.cards').html(err.error);
-    });
+    app.etsy.userDetail(url.params.userId)
+      .done(function (data) {
+        $('.cards').html(profileTemplate(data.results[0]));
+      })
+      .fail(function (req, status, err) {
+        console.log('Failed');
+        console.log(err);
+        $('.cards').html(err.error);
+      });
 
-});
-};
+  });
+}
